44defd37e02977d46dda68e5ae6e6f065be7ee02
Mike Byrd <122894342+iByrd@users.noreply.github.com>
2023-11-07 12:55:01 -0500

    Setup background image, welcome text, and hero gif

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const textToType = "Welcome to GameSphere! Do you have amazing memories playing video games...........? Are you inspired by the adventures you have had saving a princess, or defeating your greatest foe...?";
+
+const textElement = document.getElementById("welcome-text");
+
+function typeText(text, index = 0) {
+    if (index < text.length) {
+        textElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(() => typeText(text, index), 50); 
+    }
+}
+
+window.onload = () => {
+    typeText(textToType);
+};
