0404db07686009fb72c27c19f99bd919997b78c9
Brayden Cummins <cummijb@mail.uc.edu>
2023-11-29 20:52:56 -0500

    Adding About Us Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace GameSphere.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
